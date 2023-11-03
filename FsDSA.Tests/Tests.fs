afcbf92fcd331f76ae565e84c99f854edff1f448
karanveersp <karanv41@proton.me>
2023-11-03 10:40:24 -0400

    First commit

---
+++ FsDSA.Tests/Tests.fs
@@ -0,0 +1,52 @@
+module Tests
+
+open Xunit
+open FsDSA.BinaryTree
+open FsDSA.Algos
+open FsDSA
+
+
+[<Fact>]
+let ``Preorder Binary Tree Traversal Works`` () =
+    let btree = Node(1, Node(2, Empty, Empty), Node(3, Empty, Empty))
+    let result: int list = preorder btree
+
+    Assert.Equal<int list>([ 1; 2; 3 ], result)
+
+[<Fact>]
+let ``Inorder Binary Tree Traversal Works`` () =
+    let btree = Node(1, Node(2, Empty, Empty), Node(3, Empty, Empty))
+    let result = inorder btree
+    Assert.Equal<int list>([ 2; 1; 3 ], result)
+
+
+[<Fact>]
+let ``Postorder Binary Tree Traversal Works`` () =
+    let btree = Node(1, Node(2, Empty, Empty), Node(3, Empty, Empty))
+    let result = postorder btree
+    Assert.Equal<int list>([ 2; 3; 1 ], result)
+
+[<Fact>]
+let ``Binary Tree Height Works`` () =
+    let btree = Node(1, Node(2, Empty, Empty), Node(3, Empty, Node(4, Empty, Empty)))
+    let result = height btree
+    Assert.Equal(3, result)
+
+[<Fact>]
+let ``Palindrome Works`` () =
+
+    let result = isPalindrome ("Race Car")
+    Assert.True(result)
+// [<Fact>]
+// let ``Permutations Works`` () =
+//     let expected =
+//         [ [ 1; 2; 3 ]
+//           [ 1; 3; 2 ]
+//           [ 2; 1; 3 ]
+//           [ 2; 3; 1 ]
+//           [ 3; 1; 2 ]
+//           [ 3; 2; 1 ] ]
+
+//     let result = allPermutations ([ 1; 2; 3 ])
+
+//     Assert.Equal<int list list>(expected, result)
---
+++ FsDSA/Tree.fs
@@ -0,0 +1,51 @@
+﻿namespace FsDSA
+
+type BinaryTree<'a> =
+    | Empty
+    | Node of 'a * BinaryTree<'a> * BinaryTree<'a> // value, left, right
+
+module BinaryTree =
+    let rec preorder tree : 'a list =
+        match tree with
+        | Empty -> []
+        | Node (value, left, right) -> [ value ] @ (preorder left) @ (preorder right)
+
+    let rec inorder tree : 'a list =
+        match tree with
+        | Empty -> []
+        | Node (value: 'a, left, right) -> (inorder left) @ [ value ] @ (inorder right)
+
+    let rec postorder tree : 'a list =
+        match tree with
+        | Empty -> []
+        | Node (value: 'a, left, right) -> (postorder left) @ (postorder right) @ [ value ]
+
+    let rec height tree : int =
+        match tree with
+        | Empty -> 0
+        | Node (_, left, right) -> 1 + max (height left) (height right)
+
+
+    /// Check if binary tree is balanced.
+    /// A binary tree is balanced if height of the two subtrees
+    /// of every node never differs by more than 1.
+    let isBalanced tree =
+        let rec checkBalance tree =
+            match tree with
+            | Empty -> Some(0)
+            | Node (_, left, right) ->
+                match checkBalance left, checkBalance right with
+                | Some lh, Some rh when abs (lh - rh) <= 1 -> Some(1 + max lh rh)
+                | _ -> None
+
+        checkBalance tree |> Option.isSome
+
+    /// Check if a tree is a mirror of itself (symmetric).
+    let isSymmetric tree =
+        let rec isMirror t1 t2 =
+            match t1, t2 with
+            | Empty, Empty -> true
+            | Node (v1, l1, _), Node (v2, l2, r2) -> v1 = v2 && isMirror l1 r2 && isMirror l2 r2
+            | _ -> false
+
+        isMirror tree tree
