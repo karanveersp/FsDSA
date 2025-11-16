c4843df2f16a5d5d10d50e9e0172701c73f7cc51
karanveersp <karanv41@proton.me>
2025-11-16 17:40:07 -0500

    Updated merge alternately

--- FsDSA/Problems/Strings/MergeAlternately.fs
+++ FsDSA/Problems/Strings/MergeAlternately.fs
@@ -1,9 +1,9 @@
 module FsDSA.Problems.Strings.MergeAlternately
 
-// Merge Strings Alternately
-// Given two strings `word1` and `word2` merge the strings by adding letters in alternating order,
-// starting with `word1`. Append remaining letters onto the end of the merged string. Return merged string.
-let merge (a: string) (b: string) : string =
+open System.Text
+
+
+let mergeOriginal (a: string) (b: string) : string =
     let rec merger (word1: string) (word2: string) (result: string) (left: bool) : string =
         match word1, word2 with
         | "", "" -> result
@@ -18,3 +18,23 @@ let merge (a: string) (b: string) : string =
                 merger (word1.Substring 0) (word2.Substring 1) newRes true
 
     merger a b "" true
+
+/// Merge Strings Alternately
+///
+/// Given two strings `word1` and `word2` merge the strings by adding letters in alternating order,
+/// starting with `word1`. Append remaining letters onto the end of the merged string. Return merged string.
+/// https://leetcode.com/problems/merge-strings-alternately
+///
+/// This solution is O(max(m,n))
+let merge (a: string) (b: string) : string =
+    let maxLen = max a.Length b.Length
+    let res = StringBuilder()
+
+    for i in 0..maxLen do
+        if i < a.Length then
+            res.Append a[i] |> ignore
+
+        if i < b.Length then
+            res.Append b[i] |> ignore
+
+    res.ToString()
